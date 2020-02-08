039e51699fd868297c386835fe077083be56371c
memen <contactsaliens@gmail.com>
2020-02-08 11:03:38 -0500

    api cutomers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  Rails.application.routes.draw do
+    namespace :api do
+     namespace :v1 do
+      resources :customers
+     end
+    end
+   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
